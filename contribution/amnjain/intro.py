c80148b802e714bb15b39c2c65610df96d2da963
amnjain <akjaman.jain@gmail.com>
2018-10-11 01:07:35 +0530

    added my first pr intro

---
+++ contribution/amnjain/intro.py
@@ -0,0 +1,6 @@
+#Language: Python
+#Author: Aman Jain(https://github.com/amnjain)
+
+print("Hello world!")
+print("This is my first contribtion to open source on git!")
+print("This is my first PR on git as well !!")
